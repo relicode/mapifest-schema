050478b546193ff12e481529182f5cd3f36a53de
Anssi Siren <pro@avaruus.mozmail.com>
2024-09-24 12:09:35 +0300

    Remove types -dir

--- types/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { type Static } from '@sinclair/typebox';
-declare const T: import("@sinclair/typebox").TObject<{
-    x: import("@sinclair/typebox").TNumber;
-    y: import("@sinclair/typebox").TNumber;
-    z: import("@sinclair/typebox").TNumber;
-}>;
-export type T = Static<typeof T>;
-export {};
