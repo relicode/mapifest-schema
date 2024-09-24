76f86da8608164a2bb95bfcc053f6bf1023e3a45
Anssi Siren <pro@avaruus.mozmail.com>
2024-09-24 13:30:09 +0300

    Add initial event schema

--- src/schemas/mapifest-event.ts
+++ src/schemas/mapifest-event.ts
@@ -1,12 +1,15 @@
-import { Type } from '@sinclair/typebox'
+import { Type as T } from '@sinclair/typebox'
 
-import { Bounds } from './utils.js'
+import { Bounds, FromTo, Point, Tile } from './utils.js'
 
-const { Object, Optional, String } = Type
-
-export const MapifestEvent = Object({
-  id: String({ format: 'uuid' }),
-  name: String({ minLength: 3, maxLength: 128 }),
-  shortName: Optional(String({ minLength: 3, maxLength: 32 })),
+export const MapifestEvent = T.Object({
+  id: T.String({ format: 'uuid' }),
+  name: T.String({ minLength: 3, maxLength: 128 }),
+  shortName: T.Optional(T.String({ minLength: 0, maxLength: 32 })),
   bounds: Bounds,
+  center: Point,
+  time: FromTo,
+  adminIds: T.Array(T.String({ format: 'uuid' }), { minItems: 1 }),
+  description: T.String({ maxLength: 4096 }),
+  tiles: T.Array(Tile),
 })
--- src/schemas/utils.ts
+++ src/schemas/utils.ts
@@ -1,29 +1,17 @@
 import { Type as T } from '@sinclair/typebox'
 
-export const Point = T.Object({
-  lat: T.Number({ minimum: -90, maximum: 90 }),
-  lon: T.Number({ minimum: -180, maximum: 180 }),
+export const Point = T.Tuple([T.Number({ minimum: -90, maximum: 90 }), T.Number({ minimum: -180, maximum: 180 })], {
+  description: 'Latitude and longitude as tuple',
 })
 
 export const Bounds = T.Tuple([Point, Point])
 
 export const DateTime = T.String({ format: 'date-time' })
 
-export const When = T.Tuple([DateTime, DateTime])
+export const FromTo = T.Tuple([DateTime, DateTime], { description: 'Start time and end time as tuple' })
 
-/*
-const when = z.object({
-  from: timeString,
-  to: timeString,
+export const Tile = T.Object({
+  x: T.Integer(),
+  y: T.Integer(),
+  z: T.Integer(),
 })
-
-export const mapifestEvent = z
-  .object({
-    id: z.string().default(randomUUID),
-    name: z.string().default(''),
-    adminIds: z.array(z.string()).default([]),
-    description: z.string().default(''),
-    tiles: z.array(z.string()).default([]),
-  })
-  .merge(when)
-*/
