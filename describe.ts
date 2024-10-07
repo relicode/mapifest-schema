ac0e933b7a95d6f215012d7c3376a9454aeb9719
Anssi Siren <pro@avaruus.mozmail.com>
2024-10-07 16:24:00 +0300

    Output markdown from descriptions

--- describe.ts
+++ describe.ts
@@ -1,15 +1,28 @@
-import * as schemas from './src/schemas.js'
+import type { Schema } from 'yup'
+
+import * as mainSchemas from './src/schemas.js'
 import * as utilitySchemas from './src/utility-schemas.js'
 
-console.log('# Schemas')
-
-console.log('## Main')
-for (const [name, schema] of Object.entries(schemas)) {
-  console.log(name, schema)
+const wrapJs = (name: string, schema: Schema) => {
+  console.log(`### ${name}`)
+  console.log('```json')
+  console.log(JSON.stringify(schema.describe(), null, 2))
+  console.log('```')
+  console.log()
 }
 
-console.log('## Utility')
-for (const [name, schema] of Object.entries(utilitySchemas)) {
-  console.log(name, schema)
+console.log(`\
+# Schemas
+
+## Main schemas
+`)
+
+for (const [key, val] of Object.entries(mainSchemas)) {
+  wrapJs(key, val)
 }
 
+console.log('## Utility schemas\n')
+
+for (const [key, val] of Object.entries(utilitySchemas)) {
+  wrapJs(key, val)
+}
